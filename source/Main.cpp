9a2b2eec828108770f0ff8e00cb40d0be515642b
Silent <zdanio95@gmail.com>
2021-04-09 00:07:55 +0200

    First commit

---
+++ source/Main.cpp
@@ -0,0 +1,8 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	
+	return 0;
+}
---
+++ source/VersionInfo.lua
@@ -0,0 +1,8 @@
+defines { 
+    "rsc_FullName=\"Yakuza Arcade Machine Emulator\"",
+    "rsc_MinorVersion=0",
+    "rsc_RevisionID=1",
+    "rsc_BuildID=0",
+    "rsc_Copyright=\"2021\"",
+    "rsc_UpdateURL=\"RELEASE URL\""
+}
