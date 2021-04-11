2b774be100ff16c0539b6c69320d30a8289162a8
Silent <zdanio95@gmail.com>
2021-04-11 21:57:29 +0200

    Implemented csl_file_access

--- source/Y6/file_access.cpp
+++ source/Y6/file_access.cpp
@@ -34,52 +34,121 @@ class csl_file_access final : public isl_file_access
 {
 	virtual bool open(const char* path, sl::handle_t handle) override
 	{
+		HANDLE file = CreateFileW(UTF8ToWchar(path).c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+		if (file == INVALID_HANDLE_VALUE)
+		{
+			return false;
+		}
+
+		sl::file_handle_internal_t* internalHandle = sl::file_handle_instance(handle);
+		internalHandle->m_h_native = file;
+
 		return true;
 	}
 	virtual void create() override
 	{
+		// Deliberately unimplemented for now
+		assert(!"csl_file_access::create unimplemented!");
 	}
 	virtual bool remove(const char*) override
 	{
+		// Deliberately unimplemented for now
+		assert(!"csl_file_access::remove unimplemented!");
 		return false;
 	}
-	virtual bool close(sl::handle_t) override
+	virtual bool close(sl::handle_t handle) override
 	{
+		sl::file_handle_internal_t* internalHandle = sl::file_handle_instance(handle);
+		if (internalHandle != nullptr)
+		{
+			HANDLE& nativeHandle = reinterpret_cast<HANDLE&>(internalHandle->m_h_native);
+			CloseHandle(nativeHandle);
+			nativeHandle = INVALID_HANDLE_VALUE;
+			return true;
+		}
 		return false;
 	}
 	virtual bool is_exist(const char* path) override
 	{
 		return GetFileAttributesW(UTF8ToWchar(path).c_str()) != INVALID_FILE_ATTRIBUTES;
 	}
-	virtual unsigned __int64 read(sl::handle_t, void*, unsigned int) override
+	virtual int64_t read(sl::handle_t handle, void* buffer, unsigned int size) override
+	{
+		sl::file_handle_internal_t* internalHandle = sl::file_handle_instance(handle);
+		if (internalHandle != nullptr)
+		{
+			HANDLE nativeHandle = reinterpret_cast<HANDLE>(internalHandle->m_h_native);
+			if (nativeHandle != INVALID_HANDLE_VALUE)
+			{
+				DWORD numBytesRead = 0;
+				if (ReadFile(nativeHandle, buffer, size, &numBytesRead, nullptr) != FALSE)
+				{
+					internalHandle->m_file_pointer += numBytesRead;
+					return numBytesRead;
+				}
+			}
+		}
+		return -1;
+	}
+	virtual int64_t write(sl::handle_t, const void*, unsigned int) override
+	{
+		// Deliberately unimplemented for now
+		assert(!"csl_file_access::write unimplemented!");
+		return 0;
+	}
+	virtual int64_t get_size(sl::handle_t handle) override
+	{
+		int64_t size = -1;
+		const sl::file_handle_internal_t* internalHandle = sl::file_handle_instance(handle);
+		if (internalHandle != nullptr)
+		{
+			HANDLE nativeHandle = reinterpret_cast<HANDLE>(internalHandle->m_h_native);
+			if (nativeHandle != INVALID_HANDLE_VALUE)
+			{
+				LARGE_INTEGER s;
+				if (GetFileSizeEx(nativeHandle, &s) != FALSE)
+				{
+					size = s.QuadPart;
+				}
+			}
+		}
+		return size;
+	}
+	virtual int64_t seek(sl::handle_t handle, int64_t offset, sl::FILE_SEEK seekMode) override
+	{
+		int64_t newPointer = -1;
+		sl::file_handle_internal_t* internalHandle = sl::file_handle_instance(handle);
+		if (internalHandle != nullptr)
+		{
+			HANDLE nativeHandle = reinterpret_cast<HANDLE>(internalHandle->m_h_native);
+			if (nativeHandle != INVALID_HANDLE_VALUE)
+			{
+				LARGE_INTEGER newOffset;
+				LARGE_INTEGER off;
+				off.QuadPart = offset;
+				if (SetFilePointerEx(nativeHandle, off, &newOffset, seekMode != sl::FILE_SEEK_SET ? FILE_CURRENT : FILE_BEGIN) != FALSE)
+				{
+					newPointer = newOffset.QuadPart;
+					internalHandle->m_file_pointer = newOffset.QuadPart;
+				}
+			}
+		}
+		return newPointer;
+	}
+	virtual int64_t read_offset(sl::handle_t handle, void* buffer, unsigned int size, uint64_t offset) override
+	{
+		seek(handle, offset, sl::FILE_SEEK_SET);
+		return read(handle, buffer, size);
+	}
+	virtual uint64_t burst_read(sl::handle_t, void*, unsigned int, unsigned __int64) override
 	{
 		return 0;
 	}
-	virtual unsigned __int64 write(sl::handle_t, const void*, unsigned int) override
+	virtual uint64_t burst_read_flush(sl::handle_t) override
 	{
 		return 0;
 	}
-	virtual unsigned __int64 get_size(sl::handle_t) override
-	{
-		return 0;
-	}
-	virtual unsigned __int64 seek(sl::handle_t, __int64, sl::FILE_SEEK) override
-	{
-		return 0;
-	}
-	virtual unsigned __int64 read_offset(sl::handle_t, void*, unsigned int, unsigned __int64) override
-	{
-		return 0;
-	}
-	virtual unsigned __int64 burst_read(sl::handle_t, void*, unsigned int, unsigned __int64) override
-	{
-		return 0;
-	}
-	virtual unsigned __int64 burst_read_flush(sl::handle_t) override
-	{
-		return 0;
-	}
-	virtual unsigned __int64 burst_read_wait(sl::handle_t) override
+	virtual uint64_t burst_read_wait(sl::handle_t) override
 	{
 		return 0;
 	}
--- source/Y6/file_access.h
+++ source/Y6/file_access.h
@@ -2,24 +2,25 @@
 
 #include "sl.h"
 
-/* 12490 */
 class __declspec(novtable) isl_file_access
 {
 public:
 	virtual ~isl_file_access() {};
 	virtual bool open(const char* path, sl::handle_t handle) = 0;
 	virtual void create() = 0;
-	virtual bool remove(const char *) = 0;
-	virtual bool close(sl::handle_t) = 0;
+	virtual bool remove(const char*) = 0;
+	virtual bool close(sl::handle_t handle) = 0;
 	virtual bool is_exist(const char *) = 0;
-	virtual unsigned __int64 read(sl::handle_t, void *, unsigned int) = 0;
-	virtual unsigned __int64 write(sl::handle_t, const void *, unsigned int) = 0;
-	virtual unsigned __int64 get_size(sl::handle_t) = 0;
-	virtual unsigned __int64 seek(sl::handle_t, __int64, sl::FILE_SEEK) = 0;
-	virtual unsigned __int64 read_offset(sl::handle_t, void *, unsigned int, unsigned __int64) = 0;
-	virtual unsigned __int64 burst_read(sl::handle_t, void *, unsigned int, unsigned __int64) = 0;
-	virtual unsigned __int64 burst_read_flush(sl::handle_t) = 0;
-	virtual unsigned __int64 burst_read_wait(sl::handle_t) = 0;
+	virtual int64_t read(sl::handle_t handle, void* buffer, unsigned int size) = 0;
+	virtual int64_t write(sl::handle_t, const void *, unsigned int) = 0;
+	virtual int64_t get_size(sl::handle_t handle) = 0;
+	virtual int64_t seek(sl::handle_t handle, int64_t offset, sl::FILE_SEEK seekMode) = 0;
+	virtual int64_t read_offset(sl::handle_t handle, void* buffer, unsigned int size, uint64_t offset) = 0;
+
+	// These don't seem to exist in Yakuza 6, but keep them for now
+	virtual uint64_t burst_read(sl::handle_t, void *, unsigned int, unsigned __int64) = 0;
+	virtual uint64_t burst_read_flush(sl::handle_t) = 0;
+	virtual uint64_t burst_read_wait(sl::handle_t) = 0;
 	virtual void burst_status_write(unsigned int) = 0;
 	virtual bool burst_status_is_complete(unsigned int) = 0;
 	virtual void burst_status_wait(unsigned int) = 0;
