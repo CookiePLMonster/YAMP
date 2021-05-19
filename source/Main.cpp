1715648e8d9fab01987f120afced1d98f3a4bf17
Silent <zdanio95@gmail.com>
2021-05-19 21:34:14 +0200

    Initialize COM on startup

--- source/Main.cpp
+++ source/Main.cpp
@@ -4,11 +4,14 @@
 #include "RenderWindow.h"
 #include "Y6-VF5FS.h"
 
+#include "wil/com.h"
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	// TODO: This is a hack, currently shutdown crashes because of mismatched allocators
 	// Once this is handled, remove this
 	auto shutdownHack = wil::scope_exit([] { TerminateProcess(GetCurrentProcess(), 0); });
+	auto coinit = wil::CoInitializeEx(COINIT_MULTITHREADED);
 
 	HMODULE dll = Y6::VF5FS::LoadDLL();
 
